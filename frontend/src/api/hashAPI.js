f807bf7ca8d9439ac588bece86e3a5ff5053ee24
Yagyansh Singh Deshwal <yagyansh.singh.cse.2022@miet.ac.in>
2025-10-18 00:20:53 +0530

    backend_deploy

--- frontend/src/api/hashAPI.js
+++ frontend/src/api/hashAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://hashverse.onrender.com";
 
 export const getHash = async (value, algorithm) => {
   const res = await axios.get(`${API_BASE}/hash`, {
